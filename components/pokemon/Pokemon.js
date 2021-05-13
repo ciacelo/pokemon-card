a73d7ad775c8dce2a2d98cd72ec81af3fdb2197b
ciacelo <ciacelojr@gmail.com>
2021-05-13 11:42:24 -0300

    added nested-routes with NextLink

--- components/pokemon/Pokemon.js
+++ components/pokemon/Pokemon.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import styles from './style.module.scss';
+import NextLink from 'next/link';
+
+const Link = ({ index, children }) => {
+  return (
+    <NextLink href={`/pokemon/${index + 1}`}>
+      <a>{children}</a>
+    </NextLink>
+  );
+};
 
 const Pokemon = ({ pokemon }) => {
   console.log(pokemon);
@@ -7,14 +16,16 @@ const Pokemon = ({ pokemon }) => {
     <div className={styles.container}>
       <h1>Escolha seu pokemon</h1>
       <div className={styles.main}>
-        {pokemon.map((item, index) => [
-          <div className={styles.card} key={index + 1}>
-            <img className={styles.image} src={item.image} alt={item.name} />
-            <div className={styles.body}>
-              <p>{item.name}</p>
+        {pokemon.map((item, index) => (
+          <Link index={index} key={index}>
+            <div className={styles.card} key={index + 1}>
+              <img className={styles.image} src={item.image} alt={item.name} />
+              <div className={styles.body}>
+                <p>{item.name}</p>
+              </div>
             </div>
-          </div>,
-        ])}
+          </Link>
+        ))}
       </div>
     </div>
   );
