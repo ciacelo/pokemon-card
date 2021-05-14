8921b3bd5bc15e860c944a64c2941a33f7ed9bb7
ciacelo <ciacelojr@gmail.com>
2021-05-14 15:35:04 -0300

    added tests to Pokemon component

---
+++ __tests__/pokemon/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import Pokemon from '../../components/pokemon/Pokemon';
+
+describe('component <Pokemon>', () => {
+  const pokemon = [
+    {
+      image: 'aaa',
+      name: 'bulbasaur',
+      url: 'https...',
+    },
+    {
+      image: 'bbbb',
+      name: 'bulbasaur',
+      url: 'https...',
+    },
+    {
+      image: 'ccc',
+      name: 'bulbasaur',
+      url: 'https...',
+    },
+  ];
+
+  it('should have the page title', () => {
+    const { getByText } = render(<Pokemon pokemon={pokemon} />);
+    expect(getByText(/Escolha seu pokemon/)).toBeInTheDocument();
+  });
+
+  it('should have link in item of the array', async () => {
+    const { getByTestId } = render(<Pokemon pokemon={pokemon} />);
+    const elemntLinkZero = await waitForElement(() => getByTestId('link-0'));
+    expect(elemntLinkZero).toBeDefined();
+  });
+
+  it('should to show a error message when don`t have items on array', () => {
+    const { getByTestId } = render(<Pokemon pokemon={[]} />);
+    expect(getByTestId('empty-list-pokemon')).toBeDefined();
+  });
+});
--- components/pokemon/Pokemon.js
+++ components/pokemon/Pokemon.js
@@ -2,10 +2,10 @@ import React from 'react';
 import styles from './style.module.scss';
 import NextLink from 'next/link';
 
-const Link = ({ index, children }) => {
+const Link = ({ index, children, ...props }) => {
   return (
     <NextLink href={`/pokemon/${index + 1}`}>
-      <a>{children}</a>
+      <a data-testid={props.dataTestid}>{children}</a>
     </NextLink>
   );
 };
@@ -16,16 +16,26 @@ const Pokemon = ({ pokemon }) => {
     <div className={styles.container}>
       <h1>Escolha seu pokemon</h1>
       <div className={styles.main}>
-        {pokemon.map((item, index) => (
-          <Link index={index} key={index}>
-            <div className={styles.card} key={index + 1}>
-              <img className={styles.image} src={item.image} alt={item.name} />
-              <div className={styles.body}>
-                <p>{item.name}</p>
+        {pokemon.length > 0 ? (
+          pokemon.map((item, index) => (
+            <Link dataTestid={`link-${index}`} index={index} key={index}>
+              <div className={styles.card} key={index + 1}>
+                <img
+                  className={styles.image}
+                  src={item.image}
+                  alt={item.name}
+                />
+                <div className={styles.body}>
+                  <p>{item.name}</p>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))
+        ) : (
+          <h3 data-testid="empty-list-pokemon" className={styles.emptyList}>
+            Não existe Pokémon nesta arena.
+          </h3>
+        )}
       </div>
     </div>
   );
